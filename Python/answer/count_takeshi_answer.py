46f13a77ae4e972348c830bfc9cb839e16e63af8
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2023-02-12 18:00:21 +0900

    答え用と提示用を分けてみた

--- Python/answer/count_takeshi_answer.py
+++ Python/answer/count_takeshi_answer.py
@@ -1,13 +1,16 @@
 # 文字列リストclassmateから、"たけし"君が何人いるか出力する
 
+# 解答例
 def count_takeshi(classmates: list[str]):
-  # ---ここから----
+  return classmates.count("たけし")
 
+# 解答例2
+def count_takeshi2(classmates: list[str]):
+  count = 0
+  for classmate in classmates:
+    if (classmate == "たけし"):
+      count+=1
+  return count
 
-  return 
-  # ---ここまで----
-
-# いろいろ試してみよう
-# classmates = [""]
 classmates = ["たけし", "こけし", "たろう", "たけと", "たけし", "たにし", "たいじ", "たけしん", "ばなな"]
 print(count_takeshi(classmates))
--- Python/max_value_mul.py
+++ Python/max_value_mul.py
@@ -1,11 +1,16 @@
-# 整数リストnumbersから、乗算結果が最も大きくなる数字を出力する
-#
-# numbers = [1, -5, 2, 4, -9, 3]の場合、
+# 整数リストnumbersから、任意の2つの数字を選び、最も大きい乗算結果を出力する
+# 整数なので、負の数もあり得る
+# numbersの要素数は2以上である
+
+# numbers = [1, -5, -2, 7, -9, 3]の場合
 # 出力は 45
 
 def max_value_mul(numbers: list[int]):
-  numbers.sort()
-  return max(numbers[0] * numbers[1], numbers[-1] * numbers[-2])
+  # ---ここから----
 
-numbers = [1, -5, 2, 4, -9, 3]
+
+  return 
+  # ---ここまで----
+
+numbers = [1, -5, -2, 7, -9, 3]
 print(max_value_mul(numbers))

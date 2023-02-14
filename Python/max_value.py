09682b26c570de29cc44e3284435da31379d5260
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2023-02-14 17:23:21 +0900

    mdファイルを追加

--- Python/max_value.py
+++ Python/max_value.py
@@ -2,15 +2,11 @@
 
 # 解答例
 def max_value(numbers: list[int]):
-  return max(numbers)
+  # ---ここから----
 
-# 解答例2
-def max_value2(numbers: list[int]):
-  maximum = 0
-  for num in numbers:
-    if (maximum < num):
-      maximum = num
-  return maximum
 
-numbers = [1, 4, 5, 7, 8, 4, 35, 2, 12124, 2]
-max_value(numbers)
+  return 
+  # ---ここまで----
+
+numbers = [1, 4, 5, 7, 8, 4, 35, 2, 121, 2]
+print(max_value(numbers))
